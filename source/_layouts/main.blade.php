46ade78db0c90e86fc461845f4baf35940830ef4
Lukas <lukasneuschl@gmail.com>
2026-04-02 21:50:35 +0200

    Add Plausible

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -7,6 +7,13 @@
         <title>@yield('pageTitle') | Legacy Upgrade</title>
         <link rel="canonical" href="{{ $page->baseUrl }}{{ $page->getPath() ?: '/' }}">
 
+        <!-- Privacy-friendly analytics by Plausible -->
+        <script async src="https://plausible.io/js/pa-MFipDzvSeHBuUY5tCy4Ik.js"></script>
+        <script>
+            window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+            plausible.init()
+        </script>
+
         @include('_includes/og')
 
         @include('_includes/twitter')
